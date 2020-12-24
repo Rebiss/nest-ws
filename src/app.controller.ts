69f612c330981bbd5f5a3f197266932fac3e2930
rebiss <barsegyanat.93@gmail.com>
2020-12-24 20:25:17 +0400

    setup settings

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Logger, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
+  private logger = new Logger('AppController');
   constructor(private readonly appService: AppService) {}
+
+  @Post('add')
+  async accumulation(@Body('data') data: number[]): Promise<number> {
+    this.logger.log(`Adding ${data.toString()}`);
+    return this.appService.postAcumulete(data);
+  }
 
   @Get()
   getHello(): string {
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,8 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  public postAcumulete(data: number[]): number {
+    return (data || []).reduce((a: number, b: number) => a + b);
+  }
 }

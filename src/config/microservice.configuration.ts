c770b634bdc1a9d30720a60f0ed8b7dc0d075885
rebiss <barsegyanat.93@gmail.com>
2020-12-30 22:16:25 +0400

    add: ws

--- src/config/microservice.configuration.ts
+++
@@ -1,9 +0,0 @@
-import { Transport, ClientOptions } from '@nestjs/microservices';
-
-export const microserviceOptions: ClientOptions = {
-    transport: Transport.TCP,
-    options: {
-        host: 'localhost',
-        port: 3022,
-    },
-};
